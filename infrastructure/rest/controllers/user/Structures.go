d519a0f4cdc2a3345ee6d7c561b9e457ca1514ea
NanoNish <nishkarsh1206@gmail.com>
2023-08-19 06:26:17 +0530

    feat: add request types for user

---
+++ infrastructure/rest/controllers/user/Structures.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"github.com/aman-singh7/loyalty-blockchain/domain/coupon"
+)
+
+type User struct {
+
+}
+
+type PurchaseProductRequest struct {
+	UserID int `json:"uid"`
+	ProductID string `json:"productID"`
+	ProductCategory string `json:"productCategory"`
+	Coupon coupon.Coupon `json:"coupon"`
+	Tokens int `json:"tokens"`
+	TransactionID int `json:"transactionID"`
+}
+
+type PurchaseCouponRequest struct {
+	UserID int `json:"uid"`
+	Tokens int `json:"tokens"`
+	Coupon coupon.Coupon `json:"coupon"`
+	TransactionID int `json:"transactionID"`
+}
+
+type ReferralRewardRequest struct {
+	UserID int `json:"uid"`
+	Tokens int `json:"tokens"`
+	TransactionID int `json:"transactionID"`
+}
