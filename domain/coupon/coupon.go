3ea622834fe85e29aa5a6158c792b19e8e78ea24
NanoNish <nishkarsh1206@gmail.com>
2023-08-20 00:45:20 +0530

    chore: update coupon structure

--- domain/coupon/coupon.go
+++ domain/coupon/coupon.go
@@ -1,20 +1,22 @@
 package coupon
 
-type CouponType int 
+type CouponType int
+
 const (
 	CATEGORY CouponType = iota
 	UNIQUE
 )
 
 type Coupon struct {
-	IssuerBusiness string `json:"issuerBusiness" validate:"required"`
-	SuperCoins int `json:"superCoins" validate:"required"`
-	Count int `json:"count" validate:"required"`
-	Discount int `json:"discount" validate:"required"`
-	ProductCategory string `json:"productCategory"`
-    ThresholdValue int `json:"thresholdvalue"`
-	ProductId string `json:"productId"`
-    Type CouponType `json:"couponType" validate:"required"`
-    Expiry int `json:"expiry"`
-	CouponID int `json:"couponId" validate:"required"`
+	IssuerBusiness  string     `json:"issuerBusiness" validate:"required"`
+	SuperCoins      int        `json:"superCoins" validate:"required"`
+	Count           int        `json:"count" validate:"required"`
+	Discount        int        `json:"discount" validate:"required"`
+	ProductCategory int        `json:"productCategory"`
+	ThresholdValue  int        `json:"thresholdvalue"`
+	ProductId       int        `json:"productId"`
+	Type            CouponType `json:"couponType" validate:"required"`
+	ExpiryDate      int        `json:"expiryDate"`
+	CouponID        int        `json:"couponId" validate:"required"`
+	HoldingID       int        `json:"holdingId"`
 }
