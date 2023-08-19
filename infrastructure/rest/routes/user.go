7e40ac8e27f757ed09cf02ec3049f49c1bad5fe6
NanoNish <nishkarsh1206@gmail.com>
2023-08-20 03:47:26 +0530

    feat: add user routes

--- infrastructure/rest/routes/user.go
+++ infrastructure/rest/routes/user.go
@@ -9,5 +9,9 @@ func UserRoutes(g *echo.Group, controller *userController.Controller) {
 	routerUser := g.Group("/user")
 	{
 		routerUser.GET("/", controller.GetUser)
+		routerUser.GET("/discount", controller.Discount)
+		routerUser.POST("/purchase", controller.PurchaseProduct)
+		routerUser.POST("/coupon", controller.PurchaseCoupon)
+		routerUser.POST("/referral", controller.ReferralReward)
 	}
 }
