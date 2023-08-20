1956522c1c955b9ced318c91d44d4553c5ebbe5d
NanoNish <nishkarsh1206@gmail.com>
2023-08-21 01:21:45 +0530

    feat: add UIDFromJWT util

---
+++ utils/UIDFromJWT.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"net/http"
+	"github.com/aman-singh7/loyalty-blockchain/application/security/jwt"
+	"github.com/labstack/echo/v4"
+)
+
+func UIDFromJWT(token string) (string, error) {
+	claims, err := jwt.GetClaimsAndVerifyToken(token, "access")
+	if err != nil {
+		return "", echo.NewHTTPError(http.StatusBadRequest, echo.Map{"message": err.Error()})
+	}
+	return claims["id"].(string), nil
+}
